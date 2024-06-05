f4fc3dc481d5b95316f8fd7fec64c82771d80bd4
Kate Cevora <kate.wilkinson@illumr.com>
2024-06-05 14:51:37 +0100

    Tidied up plotting script, improved error handling

--- plotting.py
+++ plotting.py
@@ -5,11 +5,34 @@ import matplotlib.pyplot as plt
 import os
 
 
-class Plotter:
-    def __init__(self, csv_path=""):
+class ResultsPlotter:
+    """
+    Class of methods for plotting results - the vendi score calculated for different dataset sizes.
+    :param metric: String specifying the metric to plot.
+    :param csv_path: String path specifying then results CSV.
+    """
+    def __init__(self, metric="", csv_path=""):
+        # Check the types of the arguments are as expected
+        assert os.path.exists(csv_path), "Path {} to results CSV does not exist.".format(csv_path)
+        assert isinstance(metric, str), "Please specify the metric to be plotted, {}, as a string.".format(metric)
+
         self.csv_path = csv_path
+        self.metric = metric
 
-    def plotPixelVS(self):
+        # Run some checks on the value of metric
+        if metric == "vs_pixel":
+            self.plot_title = "MNIST Pixel Vendi Score"
+        elif metric == "vs_encoded":
+            self.plot_title = "MNIST Embed Vendi Score"
+        elif metric == "":
+            raise ValueError("Please set the metric to be plotted.")
+        else:
+            raise ValueError("The metric for plotting is not recognised.")
+
+    """
+    Method for plotting the given metric for each data category and training set size.
+    """
+    def plot(self):
         # open the csv
         results = pd.read_csv(self.csv_path)
 
@@ -28,52 +51,19 @@ class Plotter:
             values = []
             for N in n_samples:
                 # find the results matching category and number of samples
-                results_cat = results[(results["data_category"] == cat) & (results["n_samples"] == N)]["vs_pixel"].values
+                results_cat = results[(results["data_category"] == cat) & (results["n_samples"] == N)][self.metric].values
                 values.append(results_cat[0])
 
             # plot the series for a category
             ax.plot(n_samples, values, label="{}".format(cat))
 
-        ax.legend()
-        ax.set_title("MNIST Pixel Vendi Score")
+        ax.legend(title="Digit Label", loc='upper left', bbox_to_anchor=(1, 1))
+        ax.set_title(self.plot_title)
         ax.set_xticks(n_samples, n_samples)
         ax.set_xlabel("Dataset Size")
         ax.set_ylabel("Vendi Score")
         ax.set_xscale('log')
-        plt.show()
-
-    def plotEmbedVS(self):
-        # open the csv
-        results = pd.read_csv(self.csv_path)
-
-        # Find out categories our image data takes and order them
-        categories = np.sort(np.unique(results["data_category"].values))
-
-        # find out what values n_samples takes and order them
-        n_samples = np.sort(np.unique(results["n_samples"].values))
-
-        # initiate plotting
-        plt.clf()
-        ax = plt.gca()
-        # iterate through the categories and plot how the diversity score varies with dataset size
-        for cat in categories:
-            # filter data belonging to category
-            values = []
-            for N in n_samples:
-                # find the results matching category and number of samples
-                results_cat = results[(results["data_category"] == cat) & (results["n_samples"] == N)][
-                    "vs_encoded"].values
-                values.append(results_cat[0])
-
-            # plot the series for a category
-            ax.plot(n_samples, values, label="{}".format(cat))
-
-        ax.legend()
-        ax.set_title("MNIST Embed Vendi Score")
-        ax.set_xticks(n_samples, n_samples)
-        ax.set_xlabel("Dataset Size")
-        ax.set_ylabel("Vendi Score")
-        ax.set_xscale('log')
+        plt.tight_layout()
         plt.show()
 
 
@@ -111,14 +101,15 @@ class LossPlotter:
 
 
 def main():
-    plot = Plotter(csv_path="results/MNIST_Pixel_VS.csv")
-    plot.plotPixelVS()
+    plot = ResultsPlotter(csv_path="results/MNIST_Pixel_VS.csv", metric="vs_pixel")
+    plot.plot()
 
-    plot = Plotter(csv_path="results/MNIST_Embed_VS.csv")
-    plot.plotEmbedVS()
+    plot = ResultsPlotter(csv_path="results/MNIST_Embed_VS.csv", metric="vs_encoded")
+    plot.plot()
 
-    plot = Plotter(csv_path="results/MNIST_Embed_VS_Partial.csv")
-    plot.plotEmbedVS()
+    plot = ResultsPlotter(csv_path="results/MNIST_Embed_VS_Partial.csv", metric="vs_encoded")
+    plot.plot()
+
 
 if __name__ == "__main__":
     main()
