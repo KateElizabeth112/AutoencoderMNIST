Fixed averaging of errors

trainers.py
@@ -16,19 +16,25 @@ class TrainerParams:
 
 class Trainer:
     """
-    Trainer class for an Autoencoder model. Takes a model, parameters and train and test data.
+    Trainer class for an Autoencoder or classifier model. Takes a model, parameters and train and test data.
     """
-    def __init__(self, model, params, train_data, test_data):
+    def __init__(self, model, params, train_data, test_data, model_type="AE"):
         # check that the objects are instances of the correct class
         assert isinstance(params, TrainerParams), "params is not an instance of trainerParams"
         assert isinstance(model, nn.Module), "model is not an instance of nn.Module"
         assert isinstance(train_data, Dataset), "train_data is not an instance of Dataset"
         assert isinstance(test_data, Dataset), "test_data is not an instance of Dataset"
+        assert isinstance(model_type, str), "model_type must be a string"
+
+        # Run some checks on the value of model type
+        if model_type not in ("AE", "classifier"):
+            raise ValueError("The model type {} does not exist".format(self.experiment_name))
 
         self.model = model
         self.params = params
         self.train_data = train_data
         self.test_data = test_data
+        self.model_type = model_type
 
         # prepare data loaders
         self.train_loader = torch.utils.data.DataLoader(self.train_data, batch_size=self.params.batch_size,
@@ -37,12 +43,15 @@ class Trainer:
 
     def train(self):
         """
-        Trains the AE model for a number of epochs specified in self.params.n_epochs. Saves the model when training
+        Trains a model for a number of epochs specified in self.params.n_epochs. Saves the model when training
         is complete.
         :return:
         """
         # specify loss function
-        criterion = nn.MSELoss()
+        if self.model_type == "AE":
+            loss_function = nn.MSELoss()
+        elif self.model_type == "classifier":
+            loss_function = nn.CrossEntropyLoss(reduction="mean")
 
         # specify loss function
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
@@ -69,69 +78,73 @@ class Trainer:
 
         for epoch in range(1, self.params.n_epochs + 1):
             # monitor training loss
-            train_loss = 0.0
+            total_train_loss = 0.0
 
             ###################
             # train the model #
             ###################
-            for data in self.train_loader:
-                # _ stands in for labels, here
-                # no need to flatten images
-                images, _ = data
+            for images, labels in self.train_loader:
                 # clear the gradients of all optimized variables
                 optimizer.zero_grad()
                 # forward pass: compute predicted outputs by passing inputs to the model
                 preds, images_compressed = self.model(images)
                 # calculate the loss using only the first output from the network
-                loss = criterion(preds, images)
+                # loss_function returns average loss across batch
+                if self.model_type == "AE":
+                    loss = loss_function(preds, images)
+                elif self.model_type == "classifier":
+                    loss = loss_function(preds, labels)
                 # backward pass: compute gradient of the loss with respect to model parameters
                 loss.backward()
                 # perform a single optimization step (parameter update)
                 optimizer.step()
                 # update running training loss
-                train_loss += loss.item() * images.size(0)
+                # Scale it by the size of the batch since loss_function returned batch average
+                total_train_loss += loss.item() * images.size(0)
 
             # print and save training/test statistics every n_steps
             if epoch % n_steps == 0:
+
+                # calculate average training loss (divide by the size of the dataset)
+                average_train_loss = total_train_loss / len(self.train_loader.dataset)
+                print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+                    epoch,
+                    average_train_loss
+                ))
+
                 # Checkpoint model
                 torch.save({
                     'epoch': epoch,
                     'model_state_dict': self.model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict(),
-                    'loss': train_loss,
+                    'loss': average_train_loss,
                 }, self.model.save_path)
 
-                # calculate average training loss
-                train_loss = train_loss / len(self.train_loader)
-                print('Epoch: {} \tTraining Loss: {:.6f}'.format(
-                    epoch,
-                    train_loss
-                ))
-
                 # calculate test loss
-                test_loss = 0.0
-                for data in self.test_loader:
-                    # no need to flatten images
-                    images, _ = data
+                total_test_loss = 0.0
+                for images, labels in self.test_loader:
                     # forward pass: compute predicted outputs by passing inputs to the model
                     preds, images_compressed = self.model(images)
                     # calculate the loss using only the first output from the network
-                    loss = criterion(preds, images)
+                    if self.model_type == "AE":
+                        loss = loss_function(preds, images)
+                    elif self.model_type == "classifier":
+                        loss = loss_function(preds, labels)
 
-                    # update running training loss
-                    test_loss += loss.item() * images.size(0)
+                    # update running testloss
+                    total_test_loss += loss.item() * images.size(0)
 
                 # Calculate the average test loss
-                test_loss = test_loss / len(self.test_loader)
+                average_test_loss = total_test_loss / len(self.test_loader.dataset)
                 print('Epoch: {} \tTest Loss: {:.6f}'.format(
                     epoch,
-                    test_loss
+                    average_test_loss
                 ))
 
                 # store loss and epoch
                 epochs_record.append(epoch)
-                train_loss_record.append(train_loss)
-                test_loss_record.append(test_loss)
+                train_loss_record.append(average_train_loss)
+                test_loss_record.append(average_test_loss)
 
         return epochs_record, train_loss_record, test_loss_record
 
@@ -188,3 +201,53 @@ class Trainer:
         else:
             plt.savefig(os.path.join(save_path, "reconstruction.png"))
 
+
+class Inference:
+    """
+    Inference class for a trained model (autoencoder or classifer).
+    """
+    def __init__(self, model, test_data, model_type="AE"):
+        self.model = model
+        self.test_data = test_data
+        self.model_type = model_type
+
+        self.test_loader = torch.utils.data.DataLoader(self.test_data, batch_size=10, num_workers=0)
+
+    def eval(self):
+        """
+        Loads a saved model from the latest checkpoint, runs a batch of evaluation data through it and plots the
+        predictions.
+        :return:
+        """
+        # check that the model path exists
+        assert os.path.exists(self.model.save_path), "The model save path {} does not exist".format(self.model.save_path)
+
+        # load the model from the latest checkpoint
+        checkpoint = torch.load(self.model.save_path)
+
+        # Load the model state dictionary
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+
+        preds_all = []
+        labels_all = []
+
+        for images, labels in self.test_loader:
+
+            preds, _ = self.model(images)
+
+            preds = torch.argmax(preds, dim=1)
+
+            # convert to numpy arrays
+            preds = preds.detach().cpu().numpy()
+            labels = labels.detach().cpu().numpy()
+
+            # stack as a list
+            preds_all += list(preds)
+            labels_all += list(labels)
+
+        # calculate accuracy
+        error = np.array(preds_all) - np.array(labels_all)
+        error[error != 0] = 1
+        accuracy = 1 - (np.sum(error) / error.shape[0])
+
+        return accuracy