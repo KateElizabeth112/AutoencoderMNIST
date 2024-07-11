0f698724f8bb2bc3afc0b16043785d72675d068f
Kate Cevora <kate.wilkinson@illumr.com>
2024-07-11 14:26:02 +0100

    Added bash script to run generalisation experiments on HPC

---
+++ generalisationExperimentsHPC.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=15:mem=80gb:ngpus=1:gpu_type=RTX6000
+#PBS -N generalisationExperiments
+
+# bash script to run generalisation experiments on HPC
+
+cd ${PBS_O_WORKDIR}
+
+# Launch virtual environment
+module load anaconda3/personal
+
+# install requirements
+conda install --yes --file requirements.txt
+
+# generate params files
+python generateParamsFiles.py -e "Generalisation_Fixed_Entropy" -r "/rds/general/user/kc2322/home"
+
+# run experiments
+python runExperiment.py  -e "Generalisation_Fixed_Entropy" -r "/rds/general/user/kc2322/home" -s "testGeneralisation.py"
+
