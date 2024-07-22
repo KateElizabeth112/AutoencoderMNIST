03cbf5a05c9e75b14e8160aa482358b154cb8e34
Kate Cevora <kate.wilkinson@illumr.com>
2024-07-22 10:28:13 +0100

    n_samples=1000

--- generalisationExperimentsHPC.sh
+++ generalisationExperimentsHPC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -l walltime=36:00:00
+#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=15:mem=80gb:ngpus=1:gpu_type=RTX6000
-#PBS -N generalisationExperiments_500
+#PBS -N generalisationExperiments_1000
 
 # bash script to run generalisation experiments on HPC
 
